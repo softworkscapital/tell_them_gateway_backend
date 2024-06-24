b6154779fcb159d88cce7d26f27e8725c9d8eeb4
softworkscapital <160616107+softworkscapital@users.noreply.github.com>
2024-06-24 03:23:49 +0200

    Monday 24-06

--- cruds/client_profile.js
+++ cruds/client_profile.js
@@ -34,24 +34,65 @@ crudsObj.getClientById = (userId) => {
         })
     })
   };
-
+  
   // get client profile by email
-crudsObj.getClientIDByEmail = (email) => {
+  crudsObj.getClientIDByEmail = (email) => {
     return new Promise((resolve, reject)=>{
-        pool.query('SELECT client_profile_id FROM client_profile WHERE email = ?',[email], (err, results)=>{
-            if(err){
-                return reject(err);
-            }
-            return resolve(results);
-        })
+      pool.query('SELECT client_profile_id FROM client_profile WHERE email = ?',[email], (err, results)=>{
+        if(err){
+          return reject(err);
+        }
+        return resolve(results);
+      })
     })
   };
 
+  crudsObj.getClientByKey = (id, key) => {
+      return new Promise((resolve, reject)=>{
+          pool.query('SELECT * FROM client_profile WHERE client_profile_id = ? AND apikey = ?',[id, key], (err, results)=>{
+              if(err){
+                  return reject(err);
+              }
+              return resolve(results);
+          })
+      })
+    };
+  
   crudsObj.updateClient = ( client_profile_id, account_type, account_category, signed_on, name, street, surbub, city, country, phoneno1, phoneno2, email, payment_style, status) => {
     return new Promise((resolve, reject) => {
       pool.query(
         'UPDATE client_profile SET account_type = ?, account_category = ?, signed_on = ?, name = ?, house_number_and_street_name = ?, surbub = ?, city = ?, country = ?, phoneno1 = ?, phoneno2 = ?, email = ?, payment_style = ?, membershipstatus = ? WHERE client_profile_id = ?',
         [ account_type, account_category, signed_on, name, street, surbub, city, country, phoneno1, phoneno2, email, payment_style, status, client_profile_id],
+        (err, result) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve({ status: '200', message: 'update successful' });
+        }
+      );
+    });
+  };
+
+  crudsObj.configApikey = ( client_profile_id, apikey, apikey_status) => {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'UPDATE client_profile SET apikey = ?, apikey_status = ? WHERE client_profile_id = ?',
+        [ apikey, apikey_status, client_profile_id],
+        (err, result) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve({ status: '200', message: 'update successful' });
+        }
+      );
+    });
+  };
+
+  crudsObj.updateApiStatus = ( client_profile_id, apikey_status) => {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'UPDATE client_profile SET apikey_status = ? WHERE client_profile_id = ?',
+        [ apikey_status, client_profile_id],
         (err, result) => {
           if (err) {
             return reject(err);
